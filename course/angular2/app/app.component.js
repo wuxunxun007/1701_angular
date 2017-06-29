4f8826f322093ba2fa72c87a59e0f3562392b805
wanghao <1120040377@qq.com>
2017-06-29 17:28:04 +0800

    angular2

---
+++ course/angular2/app/app.component.js
@@ -0,0 +1,10 @@
+(function(app) {
+  app.AppComponent =
+    ng.core.Component({
+      selector: 'my-app',
+      template: '<h1>我的第一个 Angular 应用11111111111111111111</h1>'
+    })
+    .Class({
+      constructor: function() {}
+    });
+})(window.app || (window.app = {}));
